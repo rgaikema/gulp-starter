f62b08932bdc48618d0e290cd6f1d8f8b616b306
Remco Gaikema <rgaikema@snakeware.nl>
2019-07-11 14:56:47 +0200

    removed dummy images & updated animation

--- src/js/modules/Animations.js
+++ src/js/modules/Animations.js
@@ -3,23 +3,16 @@ import {Power2, TimelineLite} from "gsap/TweenMax";
 // Animations
 export function startAnimations() {
 	
-	// Figure animation
-	const tl = new TimelineLite(),
-		figure = document.querySelector('.figure');
-	
-	if(figure != null) {
-		tl
-			.to(figure, 2, {scale: 1.1, ease: Power2.easeInOut});
-	}
-	
-	
 	// Text animation
 	const tlText =  new TimelineLite(),
-		mySplitText = new SplitText("h1", {type:"words,chars"}),
-	chars = mySplitText.chars;
+		title = document.querySelector("h1");
 	
 	tlText
-	.staggerFrom(chars, 0.8, {opacity:0, scale:0, y:80}, 0.01, "+=0");
+		.fromTo(title, 0.8,
+			{opacity:0, scale:0, transformOrigin: "0% 50%", ease: Power2.easeInOut},
+			{opacity: 1, scale: 1, transformOrigin: "0% 50%", ease: Power2.easeInOut},
+			"+=0"
+		);
 	
 }
 
