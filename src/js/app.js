b2559245192e10293c901061637b5dc1857ab4a4
Remco <rgaikema@gmail.com>
2019-07-11 19:59:13 +0200

    added dummy filed & adjusted styling

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,5 @@ console.log('running app.js');
 import { hello } from './modules/Module.js';
 console.log(hello);
 
-const test = () => {
-	console.log('test!');
-};
-test();
-
 import { startAnimations } from './modules/Animations.js'
 startAnimations();
