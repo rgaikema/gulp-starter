d1b4e508e58f3e64bc443435505e612beff9b737
Remco <rgaikema@gmail.com>
2019-12-11 15:42:08 +0100

    added footer include & simplified h1 animation

--- src/js/modules/Animations.js
+++ src/js/modules/Animations.js
@@ -3,17 +3,20 @@ import gsap from "gsap";
 // Animations
 export function startAnimations() {
 	
-	// Text animation
-	const tlText =  new gsap.timeline(),
-		title = document.querySelector("h1");
-	
-	tlText
-		.fromTo(title,
-			{opacity:0, scale:0, transformOrigin: "0% 50%", ease: "power2.inOut"},
-			{duration: .8, opacity: 1, scale: 1, transformOrigin: "0% 50%", ease: "power2.inOut"},
-			"+=0"
-		);
-	
+	window.onload = function(){
+
+		// Text animation
+		const tlText =  new gsap.timeline(),
+			title = document.querySelector("h1");
+		
+		tlText
+			.fromTo(title,
+				{opacity:0, ease: "power2.inOut"},
+				{duration: .6, opacity: 1, ease: "power2.inOut"},
+				"+=0"
+			);
+	}
+		
 }
 
 
